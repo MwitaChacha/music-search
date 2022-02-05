c699c174911b2cc72d5626550832a8d99303ef2c
Mwita Chacha <jonahhchacha@gmail.com>
2022-02-05 12:42:04 +0300

    Changing layout of the page

--- core/views.py
+++ core/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-
+import requests
+from decouple import config
 # Create your views here.
 def index(request):
-    
-    return render(request,'index.html')
+    if 'term' in request.GET:
+        url = "https://shazam.p.rapidapi.com/search"
+        term = request.GET.get('term')
+        querystring = {"term":term,"locale":"global","offset":"0","limit":"1"}
+        
+        headers = {
+            'x-rapidapi-host': "shazam.p.rapidapi.com",
+            'x-rapidapi-key': config('x-rapidapi-key'),
+            }
+        
+        response = requests.request("GET", url, headers=headers, params=querystring).json()
+        tracks=response['tracks']
+        hits=tracks['hits']
+        track=hits[0]['track']
+        share=track['share']
+        print(response)
+    return render(request,'index.html',{'response':response,'track':track,'hits':hits,'share':share})
--- music/settings.py
+++ music/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&d3s#vuwk(tr%ya4--m&yf8^i_)ha&ke@e0ne&3f9q2x=b_6tv'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool)
 
 ALLOWED_HOSTS = []
 
